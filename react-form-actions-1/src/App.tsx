ece0e161ddd0467247aa7e1f902a887470ce5439
KapilPanchal <kapil.panchal.software@gmail.com>
2025-10-07 14:51:32 +0530

    [formaction] added event

--- react-form-actions-1/src/App.tsx
+++ react-form-actions-1/src/App.tsx
@@ -2,9 +2,16 @@ import './App.css';
 
 function App() {
 
+  const submitFormActionHandler = async(formData: FormData) => {
+    console.log("Form Submitted: ", formData.get("username"));
+  };
+
   return (
     <>
-      <h1>React Form Actions - 1</h1>
+      <form action={submitFormActionHandler}>
+        <input name='username'/>
+        <button type='submit'>Submit</button>
+      </form>
     </>
   )
 }
