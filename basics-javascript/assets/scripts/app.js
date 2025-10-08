5d54a9eb9cad587de380a3a862e4afeae57d1a17
KapilPanchal <kapil.panchal.software@gmail.com>
2025-10-08 10:17:10 +0530

    [functions] setTimeout

--- basics-javascript/assets/scripts/app.js
+++ basics-javascript/assets/scripts/app.js
@@ -137,3 +137,23 @@
 
 // const list = document.querySelector("ul");
 // list.remove();
+
+function handleTimeout () {
+  console.log("Timed Out!");
+}
+
+const handleTimeout2 = () => {
+  console.log("Timed Out2!");
+}
+
+setTimeout(handleTimeout, 2000);
+setTimeout(handleTimeout2, 3000);
+setTimeout(() => {
+  console.log("Timed Out Anonymous Function");
+}, 4000);
+
+function greeter(greetFn) {
+  greetFn();
+}
+
+greeter(() => console.log("Hello"));
