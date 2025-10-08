957ebf179230ba04fd921716b36b0811da059d83
KapilPanchal <kapil.panchal.software@gmail.com>
2025-10-08 08:15:41 +0530

    [variables] and operators example

--- basics-javascript/assets/scripts/app.js
+++ basics-javascript/assets/scripts/app.js
@@ -1,4 +1,13 @@
-import { apiKey } from "./util.js";
+// import { apiKey } from "./util.js";
+// import apiKey from "./util.js";
 
+import * as util from "./util.js";
 
-console.log(apiKey);
+console.log(util.apiKey1);
+console.log(util.apiKey2);
+console.log(util.default);
+
+const userMessage = "Hello World";
+
+console.log(userMessage);
+console.log(userMessage);
--- basics-javascript/assets/scripts/util.js
+++ basics-javascript/assets/scripts/util.js
@@ -1,1 +1,4 @@
-export let apiKey = "WERWFERFIEWRJWEJWE";
+// export let apiKey = "WERWFERFIEWRJWEJWE";
+export default "Default-WERWFERFIEWRJWEJWE";
+export let apiKey1 = "1-WERWFERFIEWRJWEJWE";
+export let apiKey2 = "2-WERWFERFIEWRJWEJWE";
