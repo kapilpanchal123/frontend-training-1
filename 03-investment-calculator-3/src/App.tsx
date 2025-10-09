39f6e1830a71247823d761d7d1918a7118025299
KapilPanchal <kapil.panchal.software@gmail.com>
2025-10-09 17:15:50 +0530

    [investment] function added

--- 03-investment-calculator-3/src/App.tsx
+++ 03-investment-calculator-3/src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css';
+import type { InvestmentInput } from './data/data';
+import { calculateInvestmentResults } from './util/investment';
 
 function App() {
 
+  const investment: InvestmentInput = {
+    initialInvestment: 15000,
+    annualInvestment: 900,
+    expectedReturn: 5.5,
+    duration: 12
+  }
+
+  const getInvestments = calculateInvestmentResults(investment);
+
   return (
     <>
+      
       <h1>Investment Calculator</h1>
+      <p>{JSON.stringify(getInvestments)}</p>
+
     </>
   )
 }
