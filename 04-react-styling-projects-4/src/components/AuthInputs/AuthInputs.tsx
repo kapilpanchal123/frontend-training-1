463d5f550112ed94a046bacc1f3a57bd8516e2c8
KapilPanchal <kapil.panchal.software@gmail.com>
2025-10-10 16:41:30 +0530

    [tailwind] added

--- 04-react-styling-projects-4/src/components/AuthInputs/AuthInputs.tsx
+++ 04-react-styling-projects-4/src/components/AuthInputs/AuthInputs.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Button from '../Button/Button';
 
 type Props = {}
 
@@ -19,36 +20,36 @@ const AuthInputs = (props: Props) => {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
-  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+  // const emailNotValid = submitted && !enteredEmail.includes('@');
+  // const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id="auth-inputs" className="w-full max-w-sm p-8 rounded mx-auto shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="controls flex flex-col gap-2 mb-6">
         <p>
-          <label>Email</label>
+          <label className="block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300">Email</label>
           <input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300"
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className="block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300">Password</label>
           <input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300"
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
         </p>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className="flex justify-end gap-4">
+        <button className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   )
