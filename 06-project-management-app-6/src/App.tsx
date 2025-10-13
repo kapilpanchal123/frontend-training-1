377e18817297202d03fac8c1f40747f3c959203c
KapilPanchal <kapil.panchal.software@gmail.com>
2025-10-13 14:52:30 +0530

    [components] added

--- 06-project-management-app-6/src/App.tsx
+++ 06-project-management-app-6/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
+import NewProject from './components/NewProject/NewProject';
+import ProjectsSidebar from './components/ProjectsSidebar/ProjectsSidebar';
 
 function App() {
 
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar />
+        <NewProject />
+      </main>
     </>
   )
 }
